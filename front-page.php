651fb36e18700d08334d6b2ae84cc4ca416bfda7
Franszo <josh@kishandchips.com>
2014-06-11 11:58:25 +0100

    parent & child page listings

--- front-page.php
+++ front-page.php
@@ -9,27 +9,47 @@
 	</div>
 </div>
 
+
+
+
+
 <section class="album-grid clearfix">
-	<?php  
-		
-		$query = new WP_Query(array(
-			'post_type' => 'albums',
-			'post_status' => 'publish',
-			'posts_per_page' => -1,
-			'caller_get_posts'=> 1				
-	    ));
-    
-	?>
-	<?php while($query->have_posts()): $query->the_post(); ?>
-	<section class="album">
-		<header>
-		<?php the_title(); ?>		
-		</header>
-		<div class="album-images">
-			
-		</div>
-	</section>
-	<?php endwhile; ?>
+
+
+<?php query_posts(array( 
+	'post_type' => 'albums',
+	'posts_per_page' => -1,
+	'post_parent' => 0
+)); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<section class="album">
+			<header>
+			<?php the_title(); ?>		
+			</header>
+			<?php 
+			        $args=array(
+			                'orderby' => 'menu_order',
+			                'order' => 'ASC',
+			                'posts_per_page' => 3,
+			                'post_type' => get_post_type( $post->ID ),
+			                'post_parent' => $post->ID
+			        );
+
+			        $childpages = new WP_Query($args);
+
+			        if($childpages->post_count > 0) {
+			            echo "<ul>";
+			            while ($childpages->have_posts()) {
+			                 $childpages->the_post();
+			                 echo "<li>".get_the_title()."</li>";
+
+			            }
+			            echo "</ul>";
+			        }
+			        
+			 ?>
+		</section>
+	<?php endwhile; endif; ?>
 </section>
 <!-- end of albums -->
 <?php get_footer(); ?>
